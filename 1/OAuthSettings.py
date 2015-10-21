b95ec9b89333fc7cf0020627324e5e5ac4a090c3
Ken Durkin <kdurkin6@fordham.edu>
2015-10-20 21:04:38 -0400

    modify auth for @TeamChris2014

--- 1/OAuthSettings.py
+++ 1/OAuthSettings.py
@@ -1,6 +1,6 @@
 settings = {
-    'consumer_key': 'cHqLphvp05Ice6WmKIN5MsNi1',
-    'consumer_secret':'fjv0m3sEGXOkghUKqNSCnPXV2epWjlToVu6Fj6A3v54otg5vmT',
-    'access_token_key': '3959711477-pnFXNByignN4g160eTHYtMnYwgg57Pfv8oMowlq',
-    'access_token_secret': '64DlWTXlnio1w2TWjiZT86MHf15DWBcIfrPxqtuODiLtA'
+    'consumer_key': 'M6LdD12bhTWuOwpeCMrUdMcN2',
+    'consumer_secret': 'CcGFTdMklYD80tb2rpX7PxhKe2FeFtPWvjnvwHRiDUxPbOvMgu',
+    'access_token_key': '2861676623-dt7a3JdZjm5Lvrn6ApJ4JmGE7K7CgLQty0CJvbj',
+    'access_token_secret': 'lYHuPLSNPKOqrVVR6sLfMkNTNeIKzATO8l176gREDP2k8'
 }
